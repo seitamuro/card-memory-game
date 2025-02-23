8dd60007fd7a1687bf668803da372bb411223941
seitamuro <seitamuro0210@gmail.com>
2025-02-23 21:29:33 +0900

    Grid上にカードを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
+import styled from "styled-components";
 import "./App.css";
 import { Card } from "./components/Card";
+
+const CardContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 3fr));
+  gap: 10px;
+`;
 
 function App() {
   useEffect(() => {
@@ -8,9 +15,17 @@ function App() {
   }, []);
 
   return (
-    <>
-      <Card />
-    </>
+    <div style={{ width: "80vw" }}>
+      <CardContainer>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </CardContainer>
+    </div>
   );
 }
 
