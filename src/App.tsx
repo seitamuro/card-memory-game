674fb283aac5aebd4d860346bd253c6d7f4a5449
seitamuro <seitamuro0210@gmail.com>
2025-02-24 00:39:40 +0900

    同じ絵柄が2枚ランダムに配置されるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { IconType } from "react-icons";
 import { FaChild, FaClock, FaSmile, FaWind } from "react-icons/fa";
 import styled from "styled-components";
 import "./App.css";
@@ -9,24 +10,46 @@ const CardContainer = styled.div`
   gap: 10px;
 `;
 
-function App() {
-  const iconList = [
-    <FaSmile size={50} />,
-    <FaClock size={50} />,
-    <FaChild size={50} />,
-    <FaWind size={50} />,
-  ];
+type CardData = {
+  icon: IconType;
+  arranged: boolean;
+};
 
-  const icons = Array.from(
-    { length: 16 },
-    () => iconList[Math.floor(Math.random() * iconList.length)]
+const createAndShuffleArray = (numbers: number[]): number[] => {
+  // 指定された数を2回ずつ含む配列を作成
+  const duplicatedArray = numbers.flatMap((num) => [num, num]);
+
+  // Fisher-Yatesアルゴリズムを使用してシャッフル
+  for (let i = duplicatedArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [duplicatedArray[i], duplicatedArray[j]] = [
+      duplicatedArray[j],
+      duplicatedArray[i],
+    ];
+  }
+
+  return duplicatedArray;
+};
+
+function App() {
+  const iconList: IconType[] = [FaSmile, FaClock, FaChild, FaWind];
+
+  const card_idx = createAndShuffleArray(
+    Array.from({ length: iconList.length }, (_, i) => i)
   );
+
+  const card_data: CardData[] = card_idx.map((idx) => {
+    return {
+      icon: iconList[idx],
+      arranged: false,
+    };
+  });
 
   return (
     <div style={{ width: "80vw" }}>
       <CardContainer>
-        {icons.map((icon) => (
-          <Card back={icon} />
+        {card_data.map((data, i) => (
+          <Card key={i} back={<data.icon size={50} />} />
         ))}
       </CardContainer>
     </div>
